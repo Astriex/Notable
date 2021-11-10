f4b63a5c674b1c233d9bb4608188b4dfa2a82a2b
Astriex <melishuskanovic@hotmail.com>
2021-11-10 10:58:25 +0100

    implemented get note use case

---
+++ app/src/main/java/com/astriex/notable/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,14 @@
+package com.astriex.notable.feature_note.domain.use_case
+
+import com.astriex.notable.feature_note.domain.model.Note
+import com.astriex.notable.feature_note.domain.repository.NoteRepository
+
+class GetNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(noteId: Int): Note? {
+        return repository.getNoteById(noteId)
+    }
+
+}
