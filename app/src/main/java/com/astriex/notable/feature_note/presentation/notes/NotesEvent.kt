9e1fcd537acaadb3827584b6ffc465d5e7a8913b
Astriex <melishuskanovic@hotmail.com>
2021-11-10 08:51:35 +0100

    implemented notes view model, added add note use case

---
+++ app/src/main/java/com/astriex/notable/feature_note/presentation/notes/NotesEvent.kt
@@ -0,0 +1,11 @@
+package com.astriex.notable.feature_note.presentation.notes
+
+import com.astriex.notable.feature_note.domain.model.Note
+import com.astriex.notable.feature_note.domain.util.NoteOrder
+
+sealed class NotesEvent {
+    data class Order(val noteOrder: NoteOrder): NotesEvent()
+    data class Delete(val note: Note): NotesEvent()
+    object Restore: NotesEvent()
+    object ToggleOrder: NotesEvent()
+}
